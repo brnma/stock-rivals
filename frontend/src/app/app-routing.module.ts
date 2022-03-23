f63a10b851f376e1d83bf5fa98059f82cd4e1e84
sunny7dusk <nat233947@gmail.com>
2022-03-22 21:35:04 -0400

    1. Added test component 2. Added ui depencides we need 3. Testing ui dependencies with test component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { TestingComponent } from './testing/testing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: TestingComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
