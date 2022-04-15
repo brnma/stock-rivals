6ac9ab2004ad282700f059031aff2806cfa327cc
sunny7dusk <nat233947@gmail.com>
2022-04-15 18:02:36 -0400

    Added navbar implementation

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  name:String="Nate";
+  cashAmt:number = 100.0;
+  profilePic:String = "https://images.unsplash.com/photo-1560415755-bd80d06eda60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=991&q=80";
 
   constructor() { }
 
