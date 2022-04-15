5ba18fce69dd63b7b656f987083c3f8dcd029c82
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-04-15 16:42:31 -0400

    chart to home screen

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { UserService } from './../_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/user';
+import { data } from '../testing/userData';
 
 @Component({
   selector: 'app-home',
@@ -15,10 +16,24 @@ export class HomeComponent implements OnInit {
   dailyChange: Number = 1.32;
   buyingPower: Number = 145222;
 
+  //chart
+  data = data;
+  view: [number, number] = [1100, 700];
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Days';
+  yAxisLabel: string = 'Profit';
+  timeline: boolean = true;
+
   constructor(private auth: AuthService, private user: UserService) {
-    this.currUser = this.auth.getUserVal;
+    // this.currUser = this.auth.getUserVal;
     // 👇this api is exposed to use for public images
-    this.currUser.profileImage = `http://localhost:3000/imgs/${this.currUser.profileImage}`;
+    // this.currUser.profileImage = `http://localhost:3000/imgs/${this.currUser.profileImage}`;
   }
 
   ngOnInit(): void {}
