762e9796529523df5d17cbfec8d11181a1122ac8
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-04-18 12:41:59 -0400

    add trade screen

--- frontend/src/app/trade/trade.component.ts
+++ frontend/src/app/trade/trade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { data } from '../testing/data';
 
 @Component({
   selector: 'app-trade',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trade.component.scss']
 })
 export class TradeComponent implements OnInit {
+  //chart
+  data = data;
+  view: [number, number] = [1000, 500];
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Days';
+  yAxisLabel: string = 'Profit';
+  timeline: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
