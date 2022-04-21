d1b9f308fcf674766e357dfb0fb6f9bf51892e16
sunny7dusk <nat233947@gmail.com>
2022-04-20 23:12:43 -0400

    Remove () from stock controllers; it returns promises instead of callback function instead

--- backend/routes/stocks.router.js
+++ backend/routes/stocks.router.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const stocksController = require('../controllers/stocks.controller');
 
 // for trade screen
-router.get('/:symbol', stocksController.grabHistoricalData());
+router.get('/:symbol', stocksController.grabHistoricalData);
 
 // for home screen to display user stocks
-router.get('/getValues', stocksController.grabUserStocks());
+router.get('/getValues', stocksController.grabUserStocks);
 
 // for trade screen to purchase stocks
-router.post('/buy', stocksController.buyStocks());
+router.post('/buy', stocksController.buyStocks);
 
 // for trade screen to sell stocks
-router.post('/sell', stocksController.sellStocks());
+router.post('/sell', stocksController.sellStocks);
 
 module.exports = router;
