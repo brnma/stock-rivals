33cdfa7743369c144f0c618a81cea9e20f2c2691
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-05-03 15:53:21 -0400

    add getGroup to controller and update minor logic

--- backend/controllers/groups.controller.js
+++ backend/controllers/groups.controller.js
@@ -1,10 +1,11 @@
 const groupsService = require('../services/groups.service');
 
-module.exports = { joinGroup, leaveGroup, createGroup };
+module.exports = { joinGroup, leaveGroup, createGroup, getGroup };
 
 async function joinGroup(req, res, next) {
+  console.log(req.body.code);
   try {
-    const data = await groupsService.joinGroup(req.user.sub, req.params.symbol);
+    const data = await groupsService.joinGroup(req.user.sub, req.body.code);
     res.json(data);
   } catch (error) {
     res.status(400).json(error);
@@ -22,7 +23,16 @@ async function leaveGroup(req, res, next) {
 
 async function createGroup(req, res, next) {
   try {
-    const data = await groupsService.createGroup();
+    const data = await groupsService.createGroup(req.user.sub);
+    res.json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function getGroup(req, res, next) {
+  try {
+    const data = await groupsService.getGroup(req.user.sub);
     res.json(data);
   } catch (error) {
     res.status(400).json(error);
