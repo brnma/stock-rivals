8c72db2169c18e0cf5a23ce123ec11153bd13000
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-05-03 15:53:35 -0400

    add getGroupt service

--- backend/services/groups.service.js
+++ backend/services/groups.service.js
@@ -5,10 +5,12 @@ module.exports = {
   joinGroup,
   leaveGroup,
   createGroup,
-  generateGroupCode
+  generateGroupCode,
+  getGroup
 };
 
 async function joinGroup(userId, code) {
+  if (code === '') throw 'no group code';
   let user = await Users.findOne({ _id: userId });
   // console.log(user);
   await user.updateOne({ groupCode: code });
@@ -18,15 +20,22 @@ async function joinGroup(userId, code) {
 async function leaveGroup(userId) {
   let user = await Users.findOne({ _id: userId });
   await user.updateOne({ groupCode: null });
-  return 1;
+  return 'left group successfully';
 }
 
 async function createGroup(userId) {
+  console.log('my USER ID: ', userId);
   let user = await Users.findOne({ _id: userId });
   const code = await generateGroupCode();
   // update user with new group code
   await user.updateOne({ groupCode: code });
   return code;
+}
+
+async function getGroup(userId) {
+  let user = await Users.findOne({ _id: userId });
+  const code = await user.groupCode;
+  return code == null ? '' : code;
 }
 
 async function generateGroupCode() {
