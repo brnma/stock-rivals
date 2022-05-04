1398ac7a98d3453bc9e46c81c6b344e42fb1adda
Brian Mamani Balderrama <62155144+brnma@users.noreply.github.com>
2022-05-03 23:32:21 -0400

    add logic to get all users in group

--- backend/controllers/groups.controller.js
+++ backend/controllers/groups.controller.js
@@ -1,6 +1,6 @@
 const groupsService = require('../services/groups.service');
 
-module.exports = { joinGroup, leaveGroup, createGroup, getGroup };
+module.exports = { joinGroup, leaveGroup, createGroup, getGroup, getGroupUsers };
 
 async function joinGroup(req, res, next) {
   console.log(req.body.code);
@@ -33,6 +33,15 @@ async function createGroup(req, res, next) {
 async function getGroup(req, res, next) {
   try {
     const data = await groupsService.getGroup(req.user.sub);
+    res.json(data);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function getGroupUsers(req, res, next) {
+  try {
+    const data = await groupsService.getGroupUsers(req.user.sub);
     res.json(data);
   } catch (error) {
     res.status(400).json(error);
