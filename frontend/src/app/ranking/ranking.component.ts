14567bf5270d684392b80db8132bf1437c784c61
sunny7dusk <nat233947@gmail.com>
2022-05-03 23:06:16 -0400

    Fixed weird ts linting error

--- frontend/src/app/ranking/ranking.component.ts
+++ frontend/src/app/ranking/ranking.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../_models/user';
-import {UserService} from '../_services/user.service';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
 @Component({
   selector: 'app-group',
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.scss']
 })
 export class RankingComponent implements OnInit {
-
-
   user1: User = {
-    username: "John",
-    profileImage: "File",
-    email: "John@vt.edu",
+    username: 'John',
+    profileImage: 'File',
+    email: 'John@vt.edu',
     currValue: 123,
     prevValue: 122,
     rank: 0,
@@ -21,9 +19,9 @@ export class RankingComponent implements OnInit {
     buyingPower: 0
   };
   user2: User = {
-    username: "John",
-    profileImage: "File",
-    email: "John@vt.edu",
+    username: 'John',
+    profileImage: 'File',
+    email: 'John@vt.edu',
     currValue: 999, //TODO base ranking off this value
     prevValue: 888,
     rank: 0,
@@ -32,20 +30,20 @@ export class RankingComponent implements OnInit {
     buyingPower: 0
   };
   user3: User = {
-    username: "John",
-    profileImage: "File",
-    email: "John@vt.edu",
+    username: 'John',
+    profileImage: 'File',
+    email: 'John@vt.edu',
     currValue: 1500, //TODO base ranking off this value
     prevValue: 888,
     rank: 0,
     token: 'djdjdjdjjdjdjd',
     groupCode: null,
-    buyingPower:0
+    buyingPower: 0
   };
   user4: User = {
-    username: "John",
-    profileImage: "File",
-    email: "John@vt.edu",
+    username: 'John',
+    profileImage: 'File',
+    email: 'John@vt.edu',
     currValue: 11, //TODO base ranking off this value
     prevValue: 888,
     rank: 0,
@@ -55,31 +53,30 @@ export class RankingComponent implements OnInit {
   };
   users: User[] = [];
 
-  constructor(private userService: UserService) {
-  }
+  constructor(private userService: UserService) {}
   ngOnInit(): void {
     // work on here TODO
     // this.userService.getAll().subscribe()
 
+    // console.log(this.userService.getUsers());
+    // this.Users = this.userService.getUsers().sort((a, b) => b.accountValue - a.accountValue);
+    // let temp:[User, User, User, User] = [this.user1, this.user2, this.user3, this.user4];
 
-  // console.log(this.userService.getUsers());
-  // this.Users = this.userService.getUsers().sort((a, b) => b.accountValue - a.accountValue);
-   // let temp:[User, User, User, User] = [this.user1, this.user2, this.user3, this.user4];
+    // this.Users = temp.sort((a, b) => b.accountValue - a.accountValue);
 
-
-   // this.Users = temp.sort((a, b) => b.accountValue - a.accountValue);
-
-        // let i: number = 1;
-        // this.Users.map((user: User) => {
-        //   user.rank = i++;
-        // });
+    // let i: number = 1;
+    // this.Users.map((user: User) => {
+    //   user.rank = i++;
+    // });
     // @ts-ignore
 
-
     // @ts-ignore
-    this.userService.getUsers().subscribe((array: User[]) => {
+    this.userService.getUsers().subscribe(
+      (arr) => {
+        console.log(arr);
+        const array = JSON.parse(JSON.stringify(arr));
         console.log(array);
-        this.users = array.sort((a, b) => b.currValue- a.currValue);
+        this.users = array.sort((a, b) => b.currValue - a.currValue);
         let i: number = 1;
         this.users.map((user: User) => {
           user.rank = i++;
@@ -92,8 +89,4 @@ export class RankingComponent implements OnInit {
   }
 }
 
-
-
-
-    //console.log(this.Users);
-
+//console.log(this.Users);
